702e0dd4b0aaeab0aa732604f80b093cb9e9552c
Matt Newman <matt.newman@dsco.io>
2019-03-20 07:35:08 -0600

    export vpc and db values from DW cloudformation

--- cloudformation/io.js
+++ cloudformation/io.js
@@ -207,6 +207,131 @@ module.exports = {
                     "Fn::Sub": "${AWS::StackName}-Fields"
                 }
             }
+        },
+        "RedshiftAddress": {
+            "Description": "Redshift Endpoint Address",
+            "Value": {
+                "Fn::GetAtt": [
+                    "Redshift",
+                    "Endpoint.Address"
+                ]
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-RedshiftAddress"
+                }
+            }
+        },
+        "RedshiftPort": {
+            "Description": "Redshift Endpoint Port",
+            "Value": {
+                "Fn::GetAtt": [
+                    "Redshift",
+                    "Endpoint.Port"
+                ]
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-RedshiftPort"
+                }
+            }
+        },
+        "AuroraClusterAddress": {
+            "Description": "Aurora Cluster Endpoint Address",
+            "Value": {
+                "Fn::GetAtt": [
+                    "AuroraCluster",
+                    "Endpoint.Address"
+                ]
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-AuroraClusterAddress"
+                }
+            }
+        },
+        "AuroraClusterPort": {
+            "Description": "Aurora Cluster Endpoint Port",
+            "Value": {
+                "Fn::GetAtt": [
+                    "AuroraCluster",
+                    "Endpoint.Port"
+                ]
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-AuroraClusterPort"
+                }
+            }
+        },
+        "SecurityGroupId": {
+            "Description": "Security Group Id",
+            "Value": {
+                "Fn::GetAtt": [
+                    "Security",
+                    "GroupId"
+                ]
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-SecurityGroupId"
+                }
+            }
+        },
+        "LambdaSubnetA": {
+            "Description": "Lambda Subnet A",
+            "Value": {
+                "Fn::Sub": "${LambdaSubnetA}"
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-LambdaSubnetA"
+                }
+            }
+        },
+        "LambdaSubnetB": {
+            "Description": "Lambda Subnet B",
+            "Value": {
+                "Fn::Sub": "${LambdaSubnetB}"
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-LambdaSubnetB"
+                }
+            }
+        },
+        "LambdaSubnetC": {
+            "Description": "Lambda Subnet C",
+            "Value": {
+                "Fn::Sub": "${LambdaSubnetC}"
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-LambdaSubnetC"
+                }
+            }
+        },
+        "DBUsername": {
+            "Description": "Database Username",
+            "Value": {
+                "Fn::Sub": "${DBUsername}"
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-DBUsername"
+                }
+            }
+        },
+        "DBEncryptedPassword": {
+            "Description": "Database Encrypted Password",
+            "Value": {
+                "Fn::Sub": "KMS:${EncryptedPassword.Value}"
+            },
+            "Export": {
+                "Name": {
+                    "Fn::Sub": "${AWS::StackName}-DBEncryptedPassword"
+                }
+            }
         }
     }
 }
